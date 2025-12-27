c4676a90dcb294b33dc7e1714faedb72d700c30d
José Carlos Chávez <jcchavezs@gmail.com>
2025-12-28 00:29:08 +0100

    docs: adds some godoc to types

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,5 +1,11 @@
 package types
 
+// SentinelVal is a dummy type used for the sentinel method in TypedConfig.
 type SentinelVal struct{}
 
-type TypedConfig interface{ SentinelFn(SentinelVal) }
+// TypedConfig is an interface that all secret source configuration types must implement.
+type TypedConfig interface {
+	// SentinelFn is a sentinel method to ensure interface compliance can only
+	// be achieved by types within this repository.
+	SentinelFn(SentinelVal)
+}
